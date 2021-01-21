316af3a9a755ac3031802795e0d76a422736cf9e
Kash-Leslie <71522973+Kash-Leslie@users.noreply.github.com>
2021-01-21 13:04:56 -0600

    let database

--- auth.js
+++ auth.js
@@ -2,10 +2,11 @@ let config = {
     apiKey: "AIzaSyAfHf815wv6KQU4lH9u6AsNnE16ok7Truc",
     authDomain: "testing-cabf3.firebaseapp.com",
     databaseURL: "https://testing-cabf3-default-rtdb.firebaseio.com/",
-    projectId: "testing-cabf3"
+    projectId: "testing-cabf3",
   };
   // Initialize Firebase
   firebase.initializeApp(config);
+  let database = firebase.database();
   let itemsRef = firebase.database().ref("items");
 
 document.addEventListener('DOMContentLoaded', function() {
