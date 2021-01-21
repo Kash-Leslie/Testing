d00780cc6352a0892118b4ae6e511ef374be5017
Kash-Leslie <71522973+Kash-Leslie@users.noreply.github.com>
2021-01-21 12:26:54 -0600

    input into database

--- auth.js
+++ auth.js
@@ -1,13 +1,20 @@
-const signupForm = document.querySelector('#signup-form');
-signupForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  // get user info
-  const email = signupForm['signup-email'].value;
-  const password = signupForm['signup-password'].value;
+let config = {
+    apiKey: "AIzaSyAfHf815wv6KQU4lH9u6AsNnE16ok7Truc",
+    authDomain: "testing-cabf3.firebaseapp.com",
+    databaseURL: "https://testing-cabf3-default-rtdb.firebaseio.com/",
+    projectId: "testing-cabf3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const itemsRef = firebase.database().ref("items");
 
-  // sign up the user
-  auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred.user);
-     });
-});
+
+document.getElementById("save").addEventListener('click',function()
+        {
+            var lostItem = {
+              username: document.getElementById("signup-email").value,
+              password: document.getElementById("signup-password").value,
+            }
+            itemsRef.update(lostItem);
+            };
