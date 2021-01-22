8ee3cf41353010d8c08a1441f20b86d11247d2af
Kash-Leslie <71522973+Kash-Leslie@users.noreply.github.com>
2021-01-22 12:29:58 -0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,8 +1,12 @@
 let config = {
     apiKey: "AIzaSyAfHf815wv6KQU4lH9u6AsNnE16ok7Truc",
     authDomain: "testing-cabf3.firebaseapp.com",
-    databaseURL: "https://testing-cabf3-default-rtdb.firebaseio.com/",
+    databaseURL: "https://testing-cabf3-default-rtdb.firebaseio.com",
     projectId: "testing-cabf3",
+    storageBucket: "testing-cabf3.appspot.com",
+    messagingSenderId: "432340193740",
+    appId: "1:432340193740:web:fca43aa551e89948de8d69",
+    measurementId: "G-FP4LJCD33P
   };
   // Initialize Firebase
   firebase.initializeApp(config);
@@ -16,6 +20,6 @@ document.addEventListener('DOMContentLoaded', function() {
            username: document.getElementById("signup-email").value,
            password: document.getElementById("signup-password").value,
          };
-         itemsRef.set(lostItem);
+         itemsRef.push(lostItem);
          });
 });
