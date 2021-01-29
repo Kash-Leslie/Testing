8deed8236fc4e8a46ae3ddbe9d4b598be458b6c4
Kash-Leslie <71522973+Kash-Leslie@users.noreply.github.com>
2021-01-29 12:47:22 -0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -4,13 +4,13 @@ const config = {
     databaseURL: "https://testing-cabf3-default-rtdb.firebaseio.com",
     projectId: "testing-cabf3",
     storageBucket: "testing-cabf3.appspot.com",
-    messagingSenderId: "432340193740",
+    messagingSenderId: "432340193740"
   };
   // Initialize Firebase
   firebase.initializeApp(config);
   const auth = firebase.auth();
   const provider = new firebase.auth.GoogleAuthProvider();
-  const signIn = document.getElementById('sign-up')
+  const signIn = document.getElementById('sign-up');
   
   signIn.onclick = () => auth.signInWithPopup(provider);
    
