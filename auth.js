b4672de9c794c301fa11345868a0e6a20849fda9
Kash-Leslie <71522973+Kash-Leslie@users.noreply.github.com>
2021-02-01 13:09:34 -0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function login(){
   var email = document.getElementById("signup-email").value;
-  var email = document.getElementById("signup-password").value;
+  var password = document.getElementById("signup-password").value;
   firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
