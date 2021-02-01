81f23db851ab63aab6b95e0542b1ede854fa1e6d
Kash-Leslie <71522973+Kash-Leslie@users.noreply.github.com>
2021-02-01 13:05:26 -0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,13 @@
 function login(){
   var email = document.getElementById("signup-email").value;
   var email = document.getElementById("signup-password").value;
-  firebase.auth().signInWithEmailAndPassword(email, password)
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+  });
 }
 
   
